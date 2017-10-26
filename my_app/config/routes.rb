8f982289af0a9a231213b8014a6f3cb316305499
Appirits / Yuta Fujii <fujii@appirits.com>
2017-10-26 11:56:46 +0900

    add passoword-form

--- my_app/config/routes.rb
+++ my_app/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  get 'users/new'
+
+  root 'home#home'
+  get '/about', to: 'home#about'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to:  'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users
 end
